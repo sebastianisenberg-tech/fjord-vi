5da0d2523eb9ae816f7d9905b9b4b38030bb5348
sebastianisenberg-tech <sebastianisenberg@gmail.com>
2026-05-11 12:25:26 -0300

    Delete migrations directory

--- migrations/001_phase16_bootstrap.sql
+++
@@ -1,8 +0,0 @@
--- Fjord VI Phase 16 bootstrap
--- Placeholder migration structure for future Alembic integration
-
-CREATE INDEX IF NOT EXISTS idx_activity_log_created_at
-ON activity_log (created_at);
-
-CREATE INDEX IF NOT EXISTS idx_audit_log_created_at
-ON audit_log (created_at);
