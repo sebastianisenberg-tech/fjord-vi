e267b7102725633cdd11b4bbc98ddcbf9bf3f3b3
sebastianisenberg-tech <sebastianisenberg@gmail.com>
2026-05-14 09:46:13 -0300

    Add files via upload

---
+++ app/routers/admin.py
@@ -0,0 +1,5 @@
+"""Fase 5: router admin.
+
+Próximo paso: mover administración, sistema, padrón, backups y auditoría.
+"""
+ROUTER_BOUNDARY_READY = True
---
+++ app/services/backups.py
@@ -0,0 +1,5 @@
+"""Fase 5: servicio de backups y diagnóstico.
+
+Futuro contenedor de pg_dump, exportaciones, snapshots y recuperación.
+"""
+SERVICE_NAME = "backups"
